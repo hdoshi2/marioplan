1e57e0b378cf63e7efc539237c718ea377eb4e6f
Hari.Doshi <hdoshi2@gmail.com>
2019-08-24 00:26:14 -0400

    feat: added static data to ProjectList

---
+++ src/Components/projects/ProjectList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-contet grey-text text-darken-3"></div>
+        <span className="card-title">Project Title</span>
+        <p>Posted by Hari Doshi</p>
+        <p className="grey-text">3rd September, 2am</p>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-contet grey-text text-darken-3"></div>
+        <span className="card-title">Project Title</span>
+        <p>Posted by Hari Doshi</p>
+        <p className="grey-text">3rd September, 2am</p>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-contet grey-text text-darken-3"></div>
+        <span className="card-title">Project Title</span>
+        <p>Posted by Hari Doshi</p>
+        <p className="grey-text">3rd September, 2am</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
