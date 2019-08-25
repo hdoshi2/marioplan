3e33bb9aeda329a8ad48c9f3464a65f11124a744
Hari.Doshi <hdoshi2@gmail.com>
2019-08-25 00:22:50 -0400

    feat: added CreateProject form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
 
 
 
@@ -17,8 +18,8 @@ function App() {
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
           <Route path="/signin" component={SignIn} />
-          <Route path="/SignUp" component={SignUp} />
-
+          <Route path="/signUp" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
