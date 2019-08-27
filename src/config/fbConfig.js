7d8d0ea924082e0422d25e5e14eb3c03fe44bbf6
Hari.Doshi <hdoshi2@gmail.com>
2019-08-26 23:38:52 -0400

    feat: set up intial firebase link

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAPJsJrmpeecMwWvyF7yPF8nHgQw7WZ1Jo",
+  authDomain: "net-ninja-marioplan-564f5.firebaseapp.com",
+  databaseURL: "https://net-ninja-marioplan-564f5.firebaseio.com",
+  projectId: "net-ninja-marioplan-564f5",
+  storageBucket: "",
+  messagingSenderId: "64373569770",
+  appId: "1:64373569770:web:5e33c5634a69535e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
